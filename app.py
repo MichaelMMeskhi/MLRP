18a628ae4e03e75be85657fa918ffb1ca5b1e3fb
Mikhail Mekhedkin Meskhi <michael.m.meskhi@live.com>
2017-04-22 20:25:25 -0500

    Pre-alpha staging

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ def upload():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(port=5002  )
+    app.run(port=5003  )
--- getnames.py
+++ getnames.py
@@ -1,18 +1,38 @@
 from __future__ import print_function
 import os
 from os import walk
+import PyPDF2
 
-mypath="C:\\Users\\aemra\\Documents\\GitHub\\MLRP\\resumes\\2"
+
+def pdf_to_text(pdf):
+    
+    pdfReader = PyPDF2.PdfFileReader(pdf)
+    
+
+
+
+mypath="C:\\Users\\aemra\\Documents\\GitHub\\MLRP\\resumes\\1"
 f = []
 for (dirpath, dirnames, filenames) in walk(mypath):
     f.extend(filenames)
     break
 print(f)
 
-with open("resumes2.csv", "w") as fa:
+with open("resumes1.csv", "w") as fa:
     print("path,succes", file=fa)
     for i in range(len(f)):
-        path="".join(["resumes\\",f[i]])
-        ts=[path, "2"]
+        path="".join(["resumes\\1\\",f[i]])
+        try:
+        	pdf=open(path, "rb")
+        	pdfReader = PyPDF2.PdfFileReader(pdf)
+
+        	ts=[path, "1"]
         
-        print(",".join(ts) ,file=fa)
+        	print(",".join(ts) ,file=fa)
+        except ValueError:
+        	print("ops!")
+        
+
+
+
+
